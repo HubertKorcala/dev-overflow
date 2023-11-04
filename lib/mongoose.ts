c15bf334192ab00555fe5e8c55962c840083b9b4
HubertKorcala <korcalahubert@gmail.com>
2023-11-04 23:52:19 +0100

    fix: correct connection with mongoose

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,14 +3,16 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDatabase = async () => {
-  mongoose.set("strictQuery", false); // prevent unknown field queries
+  mongoose.set("strictQuery", true); // prevent unknown field queries
 
   if (!process.env.MONGODB_URL) return console.log("MISSING MONGODB_URL");
 
   if (isConnected) return console.log("MongoDB is already connected");
 
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devflow",
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 
