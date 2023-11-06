0255d28c91ef90c90e3690318baeec599505adee
HubertKorcala <korcalahubert@gmail.com>
2023-11-06 18:02:27 +0100

    feat: add timeStamps in Question model

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,4 +1,4 @@
-import { Schema, models, model, Document, Date } from "mongoose";
+import { Schema, model, Document, models } from "mongoose";
 
 export type TQuestion = {
   title: string;
@@ -9,20 +9,21 @@ export type TQuestion = {
   downvotes: Schema.Types.ObjectId[];
   author: Schema.Types.ObjectId;
   answers: Schema.Types.ObjectId[];
-  createdAt: Date;
 } & Document;
 
-const QuestionSchema = new Schema<TQuestion>({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
-  views: { type: Number, default: 0 },
-  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
-  createdAt: { type: Date, default: Date.now },
-});
+const QuestionSchema = new Schema<TQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  },
+  { timestamps: true }
+);
 
 const Question = models.Question || model("Question", QuestionSchema);
 
