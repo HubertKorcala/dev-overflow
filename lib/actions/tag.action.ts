94953267df4587fbe79a3a567deb8ca11d0d3b06
HubertKorcala <korcalahubert@gmail.com>
2023-11-07 23:11:05 +0100

    feat: cerate tag action

---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag, { TagType } from "@/database/tag.model";
+
+export const getTopInteractedTags = async (
+  params: GetTopInteractedTagsParams
+) => {
+  try {
+    connectToDatabase();
+
+    // eslint-disable-next-line no-unused-vars
+    const { userId, limit = 3 } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getAllTags = async (params: GetAllTagsParams) => {
+  try {
+    connectToDatabase();
+
+    const tags: TagType[] = await Tag.find({});
+
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
