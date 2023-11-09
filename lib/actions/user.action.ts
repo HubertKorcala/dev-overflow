9b2d2b74571cc10596492dc346895f5aa4275585
HubertKorcala <korcalahubert@gmail.com>
2023-11-09 15:49:12 +0100

    feat: add toggleSaveQuestion user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,6 +6,7 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
@@ -91,6 +92,41 @@ export const getAllUsers = async (params: GetAllUsersParams) => {
     const users: TUser[] = await User.find({}).sort({ joinedAt: -1 });
 
     return { users };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const toggleSaveQuestion = async (params: ToggleSaveQuestionParams) => {
+  try {
+    connectToDatabase();
+
+    const { userId, questionId, path } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User nof found");
+    }
+
+    const isQuestionSaved = user.saved.includes(questionId);
+
+    if (isQuestionSaved) {
+      await User.findByIdAndUpdate(
+        userId,
+        { $pull: { saved: questionId } },
+        { new: true }
+      );
+    } else {
+      await User.findByIdAndUpdate(
+        userId,
+        { $addToSet: { saved: questionId } },
+        { new: true }
+      );
+    }
+
+    revalidatePath(path);
   } catch (err) {
     console.log(err);
     throw err;
