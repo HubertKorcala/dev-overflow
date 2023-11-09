3365da33f814b16600949413554337bc76370356
HubertKorcala <korcalahubert@gmail.com>
2023-11-09 22:48:33 +0100

    feat: add getSavedQuestions action for user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,16 +1,18 @@
 "use server";
-
+import { FilterQuery } from "mongoose";
 import User, { TUser } from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
 
 export async function getUserById(params: { userId: string }) {
   try {
@@ -127,6 +129,41 @@ export const toggleSaveQuestion = async (params: ToggleSaveQuestionParams) => {
     }
 
     revalidatePath(path);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export const getSavedQuestions = async (params: GetSavedQuestionsParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const getSavedQuestions = user.saved;
+
+    return { questions: getSavedQuestions };
   } catch (err) {
     console.log(err);
     throw err;
