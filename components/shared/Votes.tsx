edbad69f7730c681dde89c2fd46f0dba1202fece
HubertKorcala <korcalahubert@gmail.com>
2023-11-10 13:14:58 +0100

    feat: add count views in Votes

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import { viewQuestion } from "@/lib/actions/interaction.action";
 import {
   downvoteQuestion,
   upvoteQuestion,
@@ -7,8 +8,8 @@ import {
 import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 type Props = {
   type: "question" | "answer";
@@ -32,6 +33,7 @@ const Votes = ({
   hasSaved,
 }: Props) => {
   const path = usePathname();
+  const router = useRouter();
 
   const handleSave = async () => {
     await toggleSaveQuestion({
@@ -86,6 +88,13 @@ const Votes = ({
       }
     }
   };
+
+  useEffect(() => {
+    viewQuestion({
+      questionId: JSON.parse(itemId),
+      userId: userId ? JSON.parse(userId) : undefined,
+    });
+  }, [itemId, userId, path, router]);
 
   return (
     <div className="flex gap-5">
