d5b049263df2a6f353170287466d49aa31c17b3f
HubertKorcala <korcalahubert@gmail.com>
2023-11-10 15:29:05 +0100

    feat: create tag details page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,61 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { TQuestion } from "@/database/question.model";
+import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import { URLProps } from "@/types/types";
+import React from "react";
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  console.log(params.id);
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag questions..."
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: TQuestion) => (
+            <QuestionCard
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+              key={question._id}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There`s no tag questions to show"
+            description=" Be the first to break the silence! 🚀 Ask a Question and kickstart the
+      discussion. Our query could be the next big thing others learn from. Get
+      involved! 💡"
+            link="/"
+            linkTitle="All Questions"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
