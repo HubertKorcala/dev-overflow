feee0c199b406d80c164655336cf52f8b456986b
HubertKorcala <korcalahubert@gmail.com>
2023-11-15 23:22:47 +0100

    feat: implement pagination for AllAnswers

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,7 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter, page = 1, pageSize = 3 } = params;
+    const { searchQuery, filter, page = 1, pageSize = 8 } = params;
 
     const skipAmount = (page - 1) * pageSize;
 
